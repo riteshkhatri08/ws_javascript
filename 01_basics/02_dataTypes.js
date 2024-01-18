7e149baa9ce24b7f37ec2279ed8c597c8da9dc9f
Ritesh Khatri <riteshkhatri1512@gmail.com>
2024-01-19 05:19:23 +0530

    datatypes in javascript

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; // TO use newer version of JS
+
+// alert(3+3) // Won't work as we are using node.js instead of a browser
+
+console.log(3 + 3);
+console.log("Ritesh");
+
+let name = "ritesh";
+let age = 25;
+let isLoggedIn = false;
+
+// PRIMITIVE DATA TYPES
+// String :-  ""
+// Number :-  2 ^ 53 range
+// boolean :- true or false
+// undefined - no value is given
+// symbol :- unqiue
+
+//OBJECT
+// null - this is a also a type in java, represents empty value, it is an object
+
+console.log(typeof "TEST"); // string
