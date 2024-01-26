fe29d9d5fdb377a94544e222e61ecd62b29db0e3
Ritesh Khatri <riteshkhatri1512@gmail.com>
2024-01-27 02:30:25 +0530

    scopes and functions

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -22,3 +22,60 @@ console.log(c);
 // const is scoped
 
 // ! global scope is different for browser and node env scope
+
+/* ========================================================================= */
+// MORE ON SCOPES
+
+// ! nested scopes
+
+function one() {
+  const username = "ritesh";
+
+  function two() {
+    // ! function inside a function :O
+    // :* java developers going crazy
+    const website = "github";
+    console.log(username);
+  }
+  // invoke two
+  two();
+
+  // cannot access this website wont be defined becuase it belongs
+  // to inner function
+  //   console.log(website);
+}
+
+one();
+
+// nested functions are like inner classes in java
+// inner function can access everything from outer function
+// but outer function cannot access anything from inner function
+
+/ *=================== */;
+
+if (true) {
+  const username = "ritesh";
+  if (username === "ritesh") {
+    const website = " youtube";
+    console.log(username + website);
+  }
+  // wont work
+  //  ! console.log(website);
+}
+
+/* ********* interesting *********** */
+
+// THIS IS CALLED AS BASIC FUNCTION
+function addone(num) {
+  return num + 1;
+}
+
+console.log(addone(5));
+
+// THIS IS CALLED AN EXPRESSION
+const addTwo = function (num) {
+  return num + 2;
+};
+
+// Expression must be declared and defined before being use
+console.log(addTwo(5));
