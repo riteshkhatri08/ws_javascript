0f5b8e5afe8a6ae1a1930392e6b1037770a2bcec
Ritesh Khatri <riteshkhatri1512@gmail.com>
2024-01-27 18:15:13 +0530

    Learning Dom query selector

---
+++ 05_dom/01_queryselector.js
@@ -0,0 +1,7 @@
+// Change bg color to black on elements with class 'body-bg-color'
+document.querySelector(".body-bg-color").style.backgroundColor = "#212121";
+
+// change text color of elements with class 'body-bg-color' to white
+document.querySelector(".body-bg-color").style.color = "#fff";
+
+
