3c139155a42c188b268ff2f880a5dfdf51c03731
Ritesh Khatri <riteshkhatri1512@gmail.com>
2024-01-27 13:29:43 +0530

    basic loops in js

---
+++ 04_iterations/one.js
@@ -0,0 +1,33 @@
+// for loop
+
+for (let index = 0; index < 10; index++) {
+  let element = index;
+  console.log(element);
+}
+// console.log(element);
+
+// break and continue
+for (let index = 0; index < 10; index++) {
+  if (index === 5) {
+    continue;
+  } else if (index === 7) {
+    break;
+  }
+
+  let element = index;
+  console.log(element);
+}
+
+/// while loop
+let i = 0;
+while (i < 10) {
+  console.log(`value of index is ${i}`);
+  i++;
+}
+
+// do while loop
+do {
+  console.log(`dowhile loop index is ${index}`);
+} while (index < 10);
+
+
