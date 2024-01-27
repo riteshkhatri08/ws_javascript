9433cb071ab7f7935a86dea136cf274baf80b825
Ritesh Khatri <riteshkhatri1512@gmail.com>
2024-01-27 13:13:41 +0530

    gec

---
+++ 03_basics/05_gec.js
@@ -0,0 +1,11 @@
+// GLOBAL EXECUTION CONTEXT
+
+function addTwoNum(num1, num2) {
+  let result = num1 + num2;
+  return result;
+}
+
+let val1 = 10;
+let val2 = 5;
+let res = addTwoNum(val1, val2);
+console.log(res);
